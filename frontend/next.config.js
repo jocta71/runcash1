64079be0fcf2e7bd70cb0451aa622a8ab21d081a
Jocta <joctaxd@gmail.com>
2025-04-08 10:27:28 -0700

    asdsa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'https://runcash1-production.up.railway.app',
     NEXT_PUBLIC_SSE_SERVER_URL: process.env.NEXT_PUBLIC_SSE_SERVER_URL || 'https://runcash1-production.up.railway.app/api/events',
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://runcash5.vercel.app/api'
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    }
+    return config
   }
 }
 
