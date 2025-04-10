88b1a6a81188fe133038d6bcff581282b2f17b7a
Jocta <joctaxd@gmail.com>
2025-04-10 14:49:06 -0700

    Update Index.tsx

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -516,7 +516,7 @@ const Index = () => {
           <div className="flex flex-col lg:flex-row gap-6">
             {/* Cards de roleta à esquerda */}
             <div className="w-full lg:w-1/2">
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-4">
                 {renderRouletteCards()}
               </div>
             </div>
