8ba62f9b8bdf15820f6b4dc04b199120b5b81d74
Jocta <joctaxd@gmail.com>
2025-04-12 17:36:58 -0700

    Update RouletteCard.tsx

--- frontend/src/services/ui/components/RouletteCard/RouletteCard.tsx
+++ frontend/src/services/ui/components/RouletteCard/RouletteCard.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import NumberHistory from './NumberHistory';
+import NumberHistory from '../NumberHistory';
 import globalRouletteDataService from '../../../../services/GlobalRouletteDataService';
 import './RouletteCard.css';
 
