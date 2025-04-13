39d196d887e55192a8c2c6b4b44d95e9b3fc6ce1
Jocta <joctaxd@gmail.com>
2025-04-13 12:10:21 -0300

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,13 +5,16 @@ const Navbar = () => {
   return (
     <nav className="bg-black border-b border-gray-800 sticky top-0 z-50">
       <div className="max-w-full mx-auto px-4">
-        <div className="flex justify-between h-14 items-center">
+        <div className="flex justify-between h-12 items-center">
           {/* Logo */}
           <div className="flex-shrink-0 flex items-center">
-            <Link to="/" className="text-green-500 font-bold text-xl">
+            <Link to="/" className="text-green-500 font-bold text-lg font-mono">
               RunCash
             </Link>
           </div>
+          
+          {/* Espaço vazio (para manter o layout igual à imagem) */}
+          <div className="flex-grow"></div>
           
           {/* Autenticação e saldo */}
           <NavbarAuth />
