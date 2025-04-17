cde7e59374503c1e1e32d77c1a6893c7070527de
Jocta <joctaxd@gmail.com>
2025-04-17 20:15:28 -0300

    w

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === 'development' && 
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -45,4 +50,4 @@ export default defineConfig({
       },
     },
   }
-});
+}));
