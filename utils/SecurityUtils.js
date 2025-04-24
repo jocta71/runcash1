0fd1c4678b1c924d8e4c7628fba13fa8ab0a31b7
Usuario Exemplo <joctaxd@gmail.com>
2025-04-24 01:29:25 -0300

    a

--- utils/SecurityUtils.js
+++ utils/SecurityUtils.js
@@ -1,1 +1,19 @@
- 
+class SecurityUtils {
+  static isBlockedIP(ip) {
+    return false;
+  }
+  
+  static sanitizeData(data) {
+    return data;
+  }
+  
+  static isValidMongoId(id) {
+    return true;
+  }
+  
+  static logSecurityEvent(userId, resource, req) {
+    console.log(`Evento de segurança: ${userId} em ${resource}`);
+  }
+}
+
+module.exports = SecurityUtils; 
