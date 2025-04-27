8ae2d09141ec20200c362c1bdd802f0c013e3df7
Usuario Exemplo <joctaxd@gmail.com>
2025-04-27 05:11:06 -0300

    Update Index.tsx

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -838,7 +838,7 @@ const Index = () => {
             </div>
             
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {renderRouletteSkeletons()}
+              {isLoading ? renderRouletteSkeletons() : renderRouletteCards()}
             </div>
           </div>
           
