b37b00732994bc2b702167cedf9de4d84aa83717
Usuario Exemplo <joctaxd@gmail.com>
2025-04-29 13:49:17 -0300

    fas

--- asaas-subscription-service/src/routes/webhookRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const webhookController = require('../controllers/webhookController');
-
-// Rota para receber webhooks do Asaas
-router.post('/asaas', webhookController.processWebhook);
-
-module.exports = router; 
